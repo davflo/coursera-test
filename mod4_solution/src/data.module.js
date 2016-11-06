2e2c1a2264b648b698c971499e313c0485321d11
davflo <floriandavid@gmx.de>
2016-11-06 18:53:43 +0100

    assignment 4 solution polishing

--- mod4_solution/src/data.module.js
+++ mod4_solution/src/data.module.js
@@ -1,6 +1,5 @@
 (function () {
     'use strict';
 
-    angular.module('data', [])
-        .constant('MenuApiBasePath', ' https://davids-restaurant.herokuapp.com/');
+    angular.module('data', []);
 })();
---
+++ mod4_solution/src/menudata-api.constant.js
@@ -0,0 +1,6 @@
+(function () {
+    'use strict';
+
+    angular.module('data')
+        .constant('MenuApiBasePath', ' https://davids-restaurant.herokuapp.com/');
+})();
